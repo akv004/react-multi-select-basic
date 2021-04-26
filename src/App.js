13c8fbff72d571c3335c4e056c989f5774f25b66
Amit Verma <akv004@gmail.com>
2021-04-26 14:28:24 -0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import "./styles.css";
+
+export default function App() {
+  const dropDown = () => {};
+  return (
+    <div className="App">
+      <button></button>
+      <div>
+        <span>
+          <label>
+            <input type="checkbox" />
+            Option 1
+          </label>
+        </span>
+
+        <span>
+          <label>
+            <input type="checkbox" />
+            Option 2
+          </label>
+        </span>
+      </div>
+    </div>
+  );
+}
