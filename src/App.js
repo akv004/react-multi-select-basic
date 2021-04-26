1aeb907f7d3ee1e5bc5bd2a06868877eb1305571
Amit Verma <akv004@gmail.com>
2021-04-26 15:50:33 -0500

    start

--- src/App.js
+++ src/App.js
@@ -4,16 +4,16 @@ export default function App() {
   const dropDown = () => {};
   return (
     <div className="App">
-      <button></button>
-      <div>
-        <span>
+      <button className="menu-btn">Menu 1</button>
+      <div className="d-none shodow rounded menu">
+        <span className="d-block menu-option">
           <label>
             <input type="checkbox" />
             Option 1
           </label>
         </span>
 
-        <span>
+        <span className="d-block menu-option">
           <label>
             <input type="checkbox" />
             Option 2
